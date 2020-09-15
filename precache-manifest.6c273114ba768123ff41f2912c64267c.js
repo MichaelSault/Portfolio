ed34753492cae238352132ef1510bc4d5e656a44
Michael Sault <michael_sault@hotmail.com>
2020-09-15 02:13:57 -0400

    Updates

--- precache-manifest.6c273114ba768123ff41f2912c64267c.js
+++ precache-manifest.6c273114ba768123ff41f2912c64267c.js
@@ -1,23 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68fed33a37406fb6e579eb8c9ab614e9",
+    "revision": "ce58924cf8009ec46347fd944916dfb5",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "dd0f0639274183b4dca6",
-    "url": "/portfolio/static/js/2.a1eacf99.chunk.js"
+    "revision": "3428cbd4147acf919e05",
+    "url": "/portfolio/static/css/main.5e22c9f1.chunk.css"
+  },
+  {
+    "revision": "214bc39e76db0ecfae02",
+    "url": "/portfolio/static/js/2.6a075f13.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio/static/js/2.a1eacf99.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.6a075f13.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f001cd59f48930fa83b0",
-    "url": "/portfolio/static/js/main.6b939b05.chunk.js"
+    "revision": "3428cbd4147acf919e05",
+    "url": "/portfolio/static/js/main.920cc3a5.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
     "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0a64d2c1662468081c41d75e137e5a86",
+    "url": "/portfolio/static/media/Economica-Bold.0a64d2c1.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
   },
   {
     "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
@@ -30,6 +42,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "56ab00e848077ea3fec713da56e69681",
     "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "d9abe4d4ebc3fd7ee3b5579a52b51a6e",
+    "url": "/portfolio/static/media/profile.d9abe4d4.png"
   },
   {
     "revision": "4256483bd1f926d9eac3bc8ff3391d09",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.47e5ad17bded3336671412b8bf64b6cb.js"
+  "/portfolio/precache-manifest.6c273114ba768123ff41f2912c64267c.js"
 );
 
 self.addEventListener('message', (event) => {
