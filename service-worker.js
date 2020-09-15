53d79f3c2dd7a2d9d0b3c3d5fcff83967e4ddda0
Michael Sault <michael_sault@hotmail.com>
2020-09-15 00:04:02 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0da11d69fbca1a6be6b699b2717e1562.js"
+  "/portfolio/precache-manifest.c5144909b4f38b8ae88852ddb63aa89e.js"
 );
 
 self.addEventListener('message', (event) => {
