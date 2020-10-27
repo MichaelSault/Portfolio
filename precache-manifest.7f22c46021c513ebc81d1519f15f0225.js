db87b9e8e9ee30f286059e542f6544f1f0fa0b66
Michael Sault <michael_sault@hotmail.com>
2020-10-26 21:41:52 -0400

    Updates

---
+++ precache-manifest.7f22c46021c513ebc81d1519f15f0225.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f8873e63b7b0f072e838e7e8027740",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11b87cbe411b781553ae",
+    "url": "./static/css/main.8904b8be.chunk.css"
+  },
+  {
+    "revision": "516514ff608e3dc8d96f",
+    "url": "./static/js/2.ec713aa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ec713aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b87cbe411b781553ae",
+    "url": "./static/js/main.b40fe5d8.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "0a64d2c1662468081c41d75e137e5a86",
+    "url": "./static/media/Economica-Bold.0a64d2c1.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "./static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "./static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "./static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "./static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "63c7eeeed017717b2f8c403b6ea1127c",
+    "url": "./static/media/profile.63c7eeee.png"
+  },
+  {
+    "revision": "4256483bd1f926d9eac3bc8ff3391d09",
+    "url": "./static/media/project1.4256483b.png"
+  },
+  {
+    "revision": "8e98e4df064eb5913d34a93a3f8c8f55",
+    "url": "./static/media/project2.8e98e4df.png"
+  },
+  {
+    "revision": "0f2961e4a77aaf0454227f0d6c528a54",
+    "url": "./static/media/project3.0f2961e4.png"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "./static/media/twitter_icon.512536cb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccce106f0d4df70db75f536092fe260c.js"
+  "./precache-manifest.7f22c46021c513ebc81d1519f15f0225.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
